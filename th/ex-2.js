6f0b36fd5d735ddbe77036d8adab93513c02ea85
Piradon <piradonleungamornnara@gmail.com>
2026-07-20 01:17:54 +0700

    feat: implement moveZeroes and romanToInt functions

--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,29 @@
 const romanToInt = function (s) {
   //Start coding here
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let current = values[s[i]];
+    let next = values[s[i+1]];
+    
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
 };
 
 const result1 = romanToInt("III"); // 3
